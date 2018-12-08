e9ea7aa8127819ab34f3cb70f56db866331864d5
sagitush <44786027+sagitush@users.noreply.github.com>
2018-12-08 19:57:00 +0200

    Add files via upload

---
+++ sheela5.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sheela5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] maarach = { -5, -10, 15, 2, 5, 100 };
+            SortArray(maarach);
+            PrintArray(maarach);
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        private static void SortArray(int[]arr)
+        {
+            int b = 0;
+            int a = 0;
+            int c=-1;
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int d = int.MaxValue;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j]<d)
+                    {
+                        c = j;
+                        d = arr[j];
+                    }
+                   
+                    
+                }
+                a = arr[i];
+                b = arr[c];
+                arr[i] = b;
+                arr[c] = a;
+
+            }
+        }
+    }
+}
